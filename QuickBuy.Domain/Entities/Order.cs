7f781cc3c75681e4a7e9346bc9f0fb723dacf6fa
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-03-23 20:37:13 -0300

    feat: finish entities definition

--- QuickBuy.Domain/Entities/Order.cs
+++ QuickBuy.Domain/Entities/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuickBuy.Domain.ValueObject;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
@@ -9,6 +10,14 @@ namespace QuickBuy.Domain.Entities
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public int UserId { get; set; }
+        public DateTime DateDelivery { get; set; }
+        public string CEP { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public int Number { get; set; }
+        public int PaymentOptionId { get; set; }
+        public PaymentOption PaymentOption { get; set; }
         public ICollection<OrderItem> Items { get; set; } = new Collection<OrderItem>();
     }
 }
---
+++ QuickBuy.Domain/ValueObject/PaymentOption.cs
@@ -0,0 +1,33 @@
+﻿using QuickBuy.Domain.Enums;
+
+namespace QuickBuy.Domain.ValueObject
+{
+    public class PaymentOption
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsBankBillet
+        {
+            get { return Id == (int)TypePaymentOptionEnum.BankBillet; }
+        }
+
+        public bool IsCreditCard
+        {
+            get { return Id == (int)TypePaymentOptionEnum.CreditCard; }
+        }
+
+        public bool IsDeposit
+        {
+            get { return Id == (int)TypePaymentOptionEnum.Deposit; }
+        }
+
+        public bool IsUndefined
+        {
+            get { return Id == (int)TypePaymentOptionEnum.Undefined; }
+        }
+    }
+}
