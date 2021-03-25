79e74ebc09105253ced6d15cb999b766ebd26de1
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-03-24 22:47:16 -0300

    feat: init config of mapping entities

---
+++ QuickBuy.Repository/Config/PaymentOptionConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuickBuy.Domain.ValueObject;
+
+namespace QuickBuy.Repository.Config
+{
+    public class PaymentOptionConfiguration : IEntityTypeConfiguration<PaymentOption>
+    {
+        public void Configure(EntityTypeBuilder<PaymentOption> builder)
+        {
+        }
+    }
+}
--- QuickBuy.Repository/Context/AppDbContext.cs
+++ QuickBuy.Repository/Context/AppDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using QuickBuy.Domain.Entities;
 using QuickBuy.Domain.ValueObject;
+using QuickBuy.Repository.Config;
 
 namespace QuickBuy.Repository.Context
 {
@@ -19,6 +20,17 @@ namespace QuickBuy.Repository.Context
         public AppDbContext(DbContextOptions options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderItemConfiguration());
+            modelBuilder.ApplyConfiguration(new PaymentOptionConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
