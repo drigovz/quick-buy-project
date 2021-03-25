39c9f2ae4981bb67cfdec3141d1e0c2e30d3bf73
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-03-24 21:59:58 -0300

    feat: config DbSets and DbContext

---
+++ QuickBuy.Repository/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickBuy.Domain.Entities;
+using QuickBuy.Domain.ValueObject;
+
+namespace QuickBuy.Repository.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrdersItens { get; set; }
+        public DbSet<PaymentOption> PaymentOptions { get; set; }
+    }
+}
